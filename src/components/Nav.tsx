529c1310fc340e1957f8c1c1e64e1a381bc84ee0
Hasan Zahid <hz06164@gmail.com>
2022-01-15 18:13:35 -0600

    getting all users from the api and listing them

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -14,17 +14,14 @@ const Nav = (props: { user: User | null }) => {
   return (
     <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
       <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-      <div className="navbar-nav">
-        <div className="nav-item text-nowrap">
-          {/* since the user can be null thus the '?' */}
-          <Link to='/profile' className="nav-link px-3">{props.user?.first_name} {props.user?.last_name}</Link>
-        </div>
-        <div className="nav-item text-nowrap">
-          <Link to='/login' className="nav-link px-3"
-            onClick={logout}
-          >Sign out</Link>
-        </div>
-      </div>
+      <ul className="my-2 my-md-0 mr-md-3">
+        <Link to={'/profile'}
+          // since the user can be null thus the '?'
+          className="p-2 text-white text-decoration-none">{props.user?.first_name} {props.user?.last_name}</Link>
+        <Link to={'/login'} className="p-2 text-white text-decoration-none"
+          onClick={async () => await axios.post('logout')}
+        >Sign out</Link>
+      </ul>
     </header>
   );
 }
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
+import { User } from '../models/user';
 
 const Users = () => {
+
+  // have to define the type of the useState array
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    (
+      async () => {
+        // getting all the ambassador users from the api
+        const {data} = await axios.get('ambassadors');
+        // setting the state of the array and populating with the ambassadors
+        setUsers(data);
+      }
+    )()
+  }, []);
+
   return (
     // everything inside Layout will get passed as child props
     <Layout> 
@@ -9,20 +26,23 @@ const Users = () => {
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1,001</td>
-            <td>random</td>
-            <td>data</td>
-            <td>placeholder</td>
-            <td>text</td>
-          </tr>
+          {/* mapping users array and showing each user */}
+          {users.map(user => {
+            return (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.email}</td>
+                <td>{user.first_name} {user.last_name}</td>
+                <td></td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </Layout>
